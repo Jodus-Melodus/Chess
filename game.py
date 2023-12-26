927c1a47d90b1ac8c010b2860528ae88a1bb5ba0
Jodus-Melodus <philipgricius@gmail.com>
2023-12-26 23:39:41 +0200

    added move generator

--- game.py
+++ game.py
@@ -1,70 +1,71 @@
-
 import itertools
 
 
 class Piece:
-    def __init__(self, color: str) -> None:
+    def __init__(self, filerank: str, color: str) -> None:
+        self.filerank = filerank
         self.color = color
         self.move_multiplier = 1
+        self.movement = ['n', 's', 'e', 'w', 'nw', 'sw', 'ne', 'se']
 
     def __repr__(self) -> str:
         return self.icon if self.color == 'black' else self.icon.upper()
 
 
 class Pawn(Piece):
-    def __init__(self, color: str) -> None:
-        super().__init__(color)
+    def __init__(self, filerank: str, color: str) -> None:
+        super().__init__(filerank, color)
         self.icon = 'p'
-        self.movement = ['nn', 'n', 'nw', 'ne']
+        self.movement = ['nn', 'n', 'nw', 'ne'] if self.color == 'white' else [
+            'ss', 's', 'sw', 'se']
 
 
 class Knight(Piece):
-    def __init__(self, color: str) -> None:
-        super().__init__(color)
+    def __init__(self, filerank: str, color: str) -> None:
+        super().__init__(filerank, color)
         self.icon = 'n'
         self.movement = ['nnw', 'nne', 'nee',
                          'nww', 'see', 'sww', 'ssw', 'sse']
 
 
 class Rook(Piece):
-    def __init__(self, color: str) -> None:
-        super().__init__(color)
+    def __init__(self, filerank: str, color: str) -> None:
+        super().__init__(filerank, color)
         self.icon = 'r'
         self.movement = ['n', 's', 'e', 'w']
         self.move_multiplier = 7
 
 
 class Bishop(Piece):
-    def __init__(self, color: str) -> None:
-        super().__init__(color)
+    def __init__(self, filerank: str, color: str) -> None:
+        super().__init__(filerank, color)
         self.icon = 'b'
         self.movement = ['nw', 'sw', 'ne', 'se']
         self.move_multiplier = 7
 
 
 class Queen(Piece):
-    def __init__(self, color: str) -> None:
-        super().__init__(color)
+    def __init__(self, filerank: str, color: str) -> None:
+        super().__init__(filerank, color)
         self.icon = 'q'
-        self.movement = ['n', 's', 'e', 'w', 'nw', 'sw', 'ne', 'se']
         self.move_multiplier = 7
 
 
 class King(Piece):
-    def __init__(self, color: str) -> None:
-        super().__init__(color)
+    def __init__(self, filerank: str, color: str) -> None:
+        super().__init__(filerank, color)
         self.icon = 'k'
-        self.movement = ['n', 's', 'e', 'w', 'nw', 'sw', 'ne', 'se']
 
 
 class Empty(Piece):
-    def __init__(self, color: str) -> None:
-        super().__init__(color)
+    def __init__(self, filerank: str, color: str) -> None:
+        super().__init__(filerank, color)
         self.icon = '.'
 
 
 class Tile:
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, filerank: str, **kwargs) -> None:
+        self.filerank = filerank
         self.color = kwargs.get('color', '')
         self.n = kwargs.get('n', None)
         self.e = kwargs.get('e', None)
@@ -75,24 +76,10 @@ class Tile:
         self.sw = kwargs.get('sw', None)
         self.nw = kwargs.get('nw', None)
 
-        self.value = Empty(self.color)
-
-    def __str__(self) -> str:
-        return f'{self.value}'
+        self.value = Empty(filerank, self.color)
 
     def __repr__(self) -> str:
-        return f'''
-value:{str(self.value)}
-color:{str(self.color)}
-n:{str(self.n)}
-s:{str(self.s)}
-w:{str(self.w)}
-e:{str(self.e)}
-nw:{str(self.nw)}
-ne:{str(self.ne)}
-sw:{str(self.sw)}
-se:{str(self.se)}
-'''
+        return f'{self.value}'
 
 
 class Move:
@@ -106,15 +93,15 @@ class Move:
 
 class ChessBoard:
     def __init__(self) -> None:
-        letters = 'abcdefgh'
-        numbers = '12345678'
+        self.letters = 'abcdefgh'
+        self.numbers = '12345678'
         self.chessboard = {}
-
-        for letter, number in itertools.product(letters, numbers):
+        for letter, number in itertools.product(self.letters, self.numbers):
             tile_name = letter + number
-            self.chessboard[tile_name] = Tile()
+            self.chessboard[tile_name] = Tile(tile_name)
 
-        for letter, number in itertools.product(letters, numbers):
+    def generate_chessboard(self) -> None:
+        for letter, number in itertools.product(self.letters, self.numbers):
             tile_name = letter + number
             tile = self.chessboard[tile_name]
 
@@ -164,23 +151,23 @@ class ChessBoard:
                     index = chr(96 + file) + str(8 - rank)
                     match char:
                         case 'p' | 'P':
-                            piece = Pawn(
-                                'white' if char.isupper() else 'black')
+                            piece = Pawn(index,
+                                         'white' if char.isupper() else 'black')
                         case 'n' | 'N':
-                            piece = Knight(
-                                'white' if char.isupper() else 'black')
+                            piece = Knight(index,
+                                           'white' if char.isupper() else 'black')
                         case 'r' | 'R':
-                            piece = Rook(
-                                'white' if char.isupper() else 'black')
+                            piece = Rook(index,
+                                         'white' if char.isupper() else 'black')
                         case 'b' | 'B':
-                            piece = Bishop(
-                                'white' if char.isupper() else 'black')
+                            piece = Bishop(index,
+                                           'white' if char.isupper() else 'black')
                         case 'q' | 'Q':
-                            piece = Queen(
-                                'white' if char.isupper() else 'black')
+                            piece = Queen(index,
+                                          'white' if char.isupper() else 'black')
                         case 'k' | 'K':
-                            piece = King(
-                                'white' if char.isupper() else 'black')
+                            piece = King(index,
+                                         'white' if char.isupper() else 'black')
 
                     self.chessboard[index].value = piece
                     file += 1
@@ -188,13 +175,109 @@ class ChessBoard:
     def generate_moves(self) -> list[Move]:
         moves = []
 
-        for filerank, piece in self.chessboard:
-            if isinstance(piece, Empty):
+        for filerank, tile in self.chessboard.items():
+            if isinstance(tile.value, Empty):
                 continue
+
+            elif isinstance(tile.value, (Bishop, Rook, Queen, King)):
+                moves.append(self.evaluate_sliding_moves(tile))
+
+            elif isinstance(tile.value, Knight):
+                moves.append(self.evaluate_knight_moves(tile))
+
+            elif isinstance(tile.value, Pawn):
+                moves.append(self.evaluate_pawn_moves(tile))
+
+        return [i for x in moves for i in x]    
+
+    def evaluate_sliding_moves(self, tile: Tile) -> list[Move]:
+        moves = []
+        piece = tile.value
+
+        for direction in piece.movement:
+            new_pos = tile
+            for _ in range(1, piece.move_multiplier + 1):
+                if new_pos:
+                    new_pos = getattr(new_pos, direction)
+                else:
+                    break
+
+                if new_pos:
+                    attacked_piece = new_pos.value
+                    if isinstance(attacked_piece, Empty):
+                        moves.append(Move(tile, new_pos))
+                    elif isinstance(attacked_piece, (Pawn, Knight, Bishop, Rook, Queen, King)):
+                        if attacked_piece.color != piece.color:
+                            moves.append(Move(tile, new_pos))
+                        break
+        return moves
+
+    def evaluate_knight_moves(self, tile: Tile) -> list[Move]:
+        moves = []
+        piece = tile.value
+
+        for direction in piece.movement:
+            directions = list(direction)
+            new_pos = tile
+            for direction in directions:
+                if new_pos:
+                    new_pos = getattr(new_pos, direction)
+                else:
+                    break
+
+            if new_pos:
+                attacked_piece = new_pos.value
+                if piece.color in (attacked_piece.color, ''):
+                    moves.append(Move(tile, new_pos))
+
+        return moves
+
+    def evaluate_pawn_moves(self, tile: Tile) -> list[Move]:
+        moves = []
+        piece = tile.value
+
+        for direction in piece.movement:
+
+            # Check if pieces can move 2 spaces otherwise skip
+            if direction == 'nn' and tile.filerank[1] != '2':
+                continue
+            if direction == 'ss' and tile.filerank[1] != '7':
+                continue
+
+            directions = list(direction)
+            new_pos = tile
+            for direction in directions:
+                if new_pos:
+                    new_pos = getattr(new_pos, direction)
+                else:
+                    break
+
+            if new_pos:
+                direction = ''.join(directions)
+                attacked_piece = new_pos.value
+
+                if direction in {'nw', 'ne', 'sw', 'se'} and isinstance(new_pos.s.value, Pawn):
+                    en_passant_piece = new_pos.s.value
+                    if en_passant_piece.color != piece.color and (
+                        en_passant_piece.color == 'black'
+                        and en_passant_piece.filerank[1] == '5'
+                        or en_passant_piece.color != 'black'
+                        and en_passant_piece.color == 'white'
+                        and en_passant_piece.filerank[1] == '4'
+                    ):
+                        moves.append(Move(tile, new_pos))
+
+                elif (direction not in ('nw', 'ne', 'sw', 'se') or attacked_piece.color not in (piece.color, '')) and (direction in 'ns' and isinstance(attacked_piece, Empty)):
+                    moves.append(Move(tile, new_pos))
+
+        return moves
 
 
 if __name__ == '__main__':
     board = ChessBoard()
-
+    board.generate_chessboard()
     board.generate_board()
+
     print(board)
+
+
